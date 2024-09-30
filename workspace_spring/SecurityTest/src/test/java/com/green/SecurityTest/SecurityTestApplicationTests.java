8e48e7fb5969fea094baafab9b817209b1bd4320
penguinnhi <tork1355@gmail.com>
2024-09-30 09:36:09 +0900

    2

---
+++ workspace_spring/SecurityTest/src/test/java/com/green/SecurityTest/SecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.SecurityTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
